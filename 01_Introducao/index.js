1bdf5d9e787b8ccc1df0f591a07dc12f4ff53f4c
Felipe Destro <fdestrocontact@gmail.com>
2024-07-12 17:07:29 -0300

    Feat: Criando um CRUD

--- 01_Introducao/index.js
+++ 01_Introducao/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?nome=NodeJS
 // Route params = /curso/2
 // Request body = {nome: 'Nodejs', tipo: 'Backend' }
@@ -23,10 +25,42 @@ const cursos = ['NodeJS', 'TypeScript', 'JavaScript']
     // })
 
 // localhost:3000/curso/index 
-    server.get('/curso/:index', (req, res) => {
+
+    server.get('/cursos', (req, res) => {
+        return res.json(cursos)
+    })
+
+    server.get('/cursos/:index', (req, res) => {
         const {index} = req.params;
 
         return res.json(cursos[index])
+    })
+
+    // Request body - criando um novo curso
+    server.post('/cursos', (req, res) => {
+        const { name } = req.body
+        cursos.push(name)
+
+        return res.json(cursos)
+    }) 
+
+    // Requsição do tipo Put - Atualizando um curso
+    server.put('/cursos/:index', (req, res) => {
+        const { index } = req.params
+        const { name } = req.body
+
+        cursos[index] = name
+        
+        return res.json(cursos);
+    })
+
+    // Excluindo algum curso
+    server.delete('/cursos/:index', (req, res) => {
+        const { index } = req.params;
+
+        cursos.splice(index, 1);
+        // return res.json(cursos);
+        return res.send()
     })
 
 server.listen(3000);
