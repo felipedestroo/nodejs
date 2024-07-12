215cda71473fb46955c4b1d04560638af42c4469
Felipe Destro <fdestrocontact@gmail.com>
2024-07-12 16:33:25 -0300

    Feat: Trabalhando com Insomnia

--- 01_Introducao/index.js
+++ 01_Introducao/index.js
@@ -6,6 +6,8 @@ const server = express();
 // Route params = /curso/2
 // Request body = {nome: 'Nodejs', tipo: 'Backend' }
 
+const cursos = ['NodeJS', 'TypeScript', 'JavaScript']
+
 // localhost:3000/curso - Query
 // server.get('/curso', (req, res) => {
 //     const nome = req.query.nome
@@ -14,10 +16,17 @@ const server = express();
 // })
 
 // localhost:3000/curso/2 - params
-    server.get('/curso/:id', (req, res) => {
-        const id = req.params.id;
+    // server.get('/curso/:id', (req, res) => {
+    //     const id = req.params.id;
 
-        return res.json({curso: `Curso: ${id}`});
+    //     return res.json({curso: `Curso: ${id}`});
+    // })
+
+// localhost:3000/curso/index 
+    server.get('/curso/:index', (req, res) => {
+        const {index} = req.params;
+
+        return res.json(cursos[index])
     })
 
 server.listen(3000);
