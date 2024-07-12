f9a21588d359d60fec99bf2e42af44dcd2c7e200
Felipe Destro <fdestrocontact@gmail.com>
2024-07-12 16:16:56 -0300

    Feat: query and param

--- 01_Introducao/index.js
+++ 01_Introducao/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const server = express();
 
-// localhost:3000/curso
-server.get('/curso', (req, res) => {
-    return res.json({curso: 'Node JS'})
-})
+// Query params = ?nome=NodeJS
+// Route params = /curso/2
+// Request body = {nome: 'Nodejs', tipo: 'Backend' }
+
+// localhost:3000/curso - Query
+// server.get('/curso', (req, res) => {
+//     const nome = req.query.nome
+
+//     return res.json({curso: `Aprendendo: ${nome}`})
+// })
+
+// localhost:3000/curso/2 - params
+    server.get('/curso/:id', (req, res) => {
+        const id = req.params.id;
+
+        return res.json({curso: `Curso: ${id}`});
+    })
 
 server.listen(3000);
