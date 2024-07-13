e00cda0c88aec8cde136947e8027b5c59a7e9283
felipedestroo <fdestrocontact@gmail.com>
2024-07-13 13:55:55 -0300

    Feat: Adicionando dados de usuarios e casas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const routes = require('./routes')
-
-class App {
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import routes from './routes';
+class App{
 
     constructor() {
         this.server = express();
+
+        mongoose.connect('mongodb+srv://felipedestro:Cetech43**@devhouse.bqz6p6w.mongodb.net/devhouse?retryWrites=true&w=majority&appName=devHouse')
 
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
-        this.server.use(express.json)
+
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'uploads'))
+        );
+
+        this.server.use(express.json());
     }
 
     routes() {
@@ -20,4 +29,4 @@ class App {
 
 }
 
-module.exports = new App().server;
+export default new App().server;
